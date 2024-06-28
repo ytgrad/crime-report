cf6a9cf54923796e67f288852a0e5090944c8929
ytgrad <ytgrad17@gmail.com>
2024-06-28 06:57:57 +0530

    fragment added

---
+++ app/src/main/java/com/example/criminal_intent/CrimeDetailFragment.kt
@@ -0,0 +1,47 @@
+package com.example.criminal_intent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import com.example.criminal_intent.databinding.FragmentCrimeDetailBinding
+import java.time.LocalDate
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment : Fragment(){
+    private lateinit var binding: FragmentCrimeDetailBinding
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.etTitle.doOnTextChanged { text, _, _, _ ->
+            crime = crime.copy(title = text.toString())
+        }
+
+    }
+
+
+}
